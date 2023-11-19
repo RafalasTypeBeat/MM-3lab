125997ece01b2be6e26e5ab91bf9313d40aeb763
Rapolas Kairys <rapolas.kairys@gmail.com>
2023-11-19 17:34:27 +0200

    updated augio

--- 3lab.py
+++ 3lab.py
@@ -34,7 +34,6 @@ X = df[features1]
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 X_scaled = pd.DataFrame(X_scaled, columns=features1)
-X_scaled['Channel'] = df['Channel']
 
 # Extract features for clustering
 features2 = ['Fresh', 'Grocery']
@@ -47,8 +46,9 @@ Xclust_scaled = pd.DataFrame(Xclust_scaled, columns=features2)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Create UMAP visualization with specified parameters
-reducer = umap.UMAP(n_neighbors=30, n_components=2, min_dist=0.1, spread=3.0, random_state=42)
-embedding = reducer.fit_transform(X)
+reducer = umap.UMAP(n_neighbors=15, n_components=2, min_dist=0.1, spread=1.0, random_state=42)
+embedding = reducer.fit_transform(X_scaled)
+X_scaled['Channel'] = df['Channel']
 
 # Create a custom colormap with red and blue
 cmap_custom = ListedColormap(['orange', 'blue'])
